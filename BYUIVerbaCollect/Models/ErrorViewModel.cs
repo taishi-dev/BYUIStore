ff5403db62cf15b4fccaf604b58a0d1c73d59a3b
BYUI Verba Collect <byui@verba.local>
2026-03-19 16:57:17 -0600

    change the application name to CampusAdoptions

--- BYUIVerbaCollect/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace BYUIVerbaCollect.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
